99757595cf88e7e4dc3775bc9efb03498d600cd3
brunomachadors <brunomachadors@gmail.com>
2023-11-12 19:11:00 +0000

    Right side components

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -1,4 +1,14 @@
-import { AnimatedText, BlackScreen, Word } from './styles';
+import {
+  AnimatedText,
+  BlackScreen,
+  BlackScreenList,
+  Button,
+  List,
+  ListText,
+  WhiteScreen,
+  Word,
+  Screen,
+} from './styles';
 
 function Display() {
   return (
@@ -7,6 +17,47 @@ function Display() {
         <Word>POKEDEX</Word>
       </AnimatedText>
     </BlackScreen>
+  );
+}
+
+import { useEffect, useState } from 'react';
+import { getPokemonList } from '../../api/pokemon/pokemons';
+import { TpokemonList, Tresult } from '../../types/pokemon';
+
+export function DisplayList() {
+  const [pokemonList, setPokemonList] = useState<Tresult[]>([]);
+
+  useEffect(() => {
+    async function getList() {
+      try {
+        const response: TpokemonList = await getPokemonList(0, 151);
+        console.log(response);
+        setPokemonList(response.results);
+      } catch (error) {
+        console.error('Error fetching Pokemon list:', error);
+      }
+    }
+
+    getList();
+  }, []);
+  return (
+    <BlackScreenList>
+      <List>
+        {pokemonList.map((pokemon) => (
+          <Button>
+            <ListText key={pokemon.name}>{pokemon.name}</ListText>
+          </Button>
+        ))}
+      </List>
+    </BlackScreenList>
+  );
+}
+
+export function DisplayMonitor() {
+  return (
+    <WhiteScreen>
+      <Screen></Screen>
+    </WhiteScreen>
   );
 }
 
--- src/components/Display/styles.tsx
+++ src/components/Display/styles.tsx
@@ -10,6 +10,7 @@ export const BlackScreen = styled.div`
   background-image: radial-gradient(#1d1e2c, #09090d);
   border: 3px solid black;
   overflow: hidden;
+  border-radius: 10px;
   margin-top: -20px;
 `;
 
@@ -27,4 +28,73 @@ export const AnimatedText = styled.div`
 export const Word = styled.div`
   display: inline-block;
   animation: ${wordAnimation} 3s linear infinite;
+`;
+
+export const BlackScreenList = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+  width: 70%;
+  height: 40%;
+  background-image: radial-gradient(#1d1e2c, #09090d);
+  border: 3px solid black;
+  overflow: hidden;
+  margin-top: -20px;
+  flex-direction: column;
+  overflow-y: auto;
+`;
+
+export const List = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  padding-inline-start: 0;
+  font-size: 20px;
+`;
+
+export const ListText = styled.li`
+  font-size: 18px;
+  color: white;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'RetroGaming', sans-serif;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  position: relative;
+  background-image: radial-gradient(#1d1e2c, #09090d);
+`;
+
+export const WhiteScreen = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 60%;
+  height: 45%;
+  background-image: radial-gradient(#f1f1f4, #bdbdbd);
+  border: 3px solid black;
+  overflow: hidden;
+  margin-top: -20px;
+  border-radius: 10px;
+`;
+
+export const Screen = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+  width: 80%;
+  height: 80%;
+  background-image: radial-gradient(#1d1e2c, #09090d);
+  border: 3px solid black;
+  overflow: hidden;
+  border-radius: 10px;
 `;
