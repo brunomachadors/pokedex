e3f12a36686f2e35cfc23087e9a45b3e268a9f86
brunomachadors <brunomachadors@gmail.com>
2023-11-15 18:28:21 +0000

    new try 66

--- src/components/Type/index.tsx
+++ src/components/Type/index.tsx
@@ -19,7 +19,7 @@ function PokemonType({ pokemon }: { pokemon: Tresult }) {
         >
           <TypeContent>
             <TypeIcon
-              src={`../../icons/${type.toLowerCase()}.svg`}
+              src={`./public/icons/${type.toLowerCase()}.svg`}
               alt={`${type} icon`}
             />
             {type.toUpperCase()}
