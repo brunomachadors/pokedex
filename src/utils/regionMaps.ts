4717e68b277489bb8fde352908abb2c9adfa711c
brunomachadors <brunomachadors@gmail.com>
2023-11-27 14:08:34 +0000

    fix image url

--- src/utils/regionMaps.ts
+++ src/utils/regionMaps.ts
@@ -1,18 +1,18 @@
 export const MAPS = {
   kanto:
-    'https://archives.bulbagarden.net/media/upload/2/25/LGPE_Kanto_Map.png',
+    'https://archives.bulbagarden.net/media/upload/thumb/2/25/LGPE_Kanto_Map.png/1200px-LGPE_Kanto_Map.png',
   johto: 'https://archives.bulbagarden.net/media/upload/6/64/JohtoMap.png',
   hoenn: 'https://archives.bulbagarden.net/media/upload/8/85/Hoenn_ORAS.png',
   sinnoh:
-    'https://archives.bulbagarden.net/media/upload/0/08/Sinnoh_BDSP_artwork.png',
+    'https://archives.bulbagarden.net/media/upload/thumb/0/08/Sinnoh_BDSP_artwork.png/800px-Sinnoh_BDSP_artwork.png',
   unova:
-    'https://archives.bulbagarden.net/media/upload/f/fc/Unova_B2W2_alt.png',
+    'https://archives.bulbagarden.net/media/upload/archive/f/fc/20160817042636%21Unova_B2W2_alt.png',
   kalos: 'https://archives.bulbagarden.net/media/upload/8/8a/Kalos_alt.png',
   alola:
     'https://archives.bulbagarden.net/media/upload/0/0b/Alola_USUM_artwork.png',
   galar: 'https://archives.bulbagarden.net/media/upload/c/ce/Galar_artwork.png',
   hisui:
-    'https://archives.bulbagarden.net/media/upload/2/22/Legends_Arceus_Hisui.png',
+    'https://archives.bulbagarden.net/media/upload/thumb/2/22/Legends_Arceus_Hisui.png/800px-Legends_Arceus_Hisui.png',
   paldea:
     'https://archives.bulbagarden.net/media/upload/f/fd/Paldea_artwork.png',
 };
